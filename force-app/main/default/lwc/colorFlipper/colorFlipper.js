5a18223d560c89cbf63c122a753db7e4e2a444cc
Scott Fenton <sfenton3@users.noreply.github.com>
2022-06-22 19:19:38 -0400

    Revert "."
    This reverts commit 2e98706a8ad44d69720227e24e4e3aa0564791e3.

--- force-app/main/default/lwc/colorFlipper/colorFlipper.js
+++ force-app/main/default/lwc/colorFlipper/colorFlipper.js
@@ -1,12 +1,3 @@
 import { LightningElement } from 'lwc';
 
-//a module can export a single default function or variable
-//class is syntatic sugar for a constructory function
-export default class ColorFlipper extends LightningElement {
-    
-    color = 'red';
-    simple = 'background-color: red';
-
-
-
-}
+export default class ColorFlipper extends LightningElement {}
--- force-app/main/default/lwc/test/test.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, track } from 'lwc';
-
-export default class App extends LightningElement {
-    @track outputText;
-    updateText(event) {
-        //this.outputText = this.template.querySelector('lightning-input').value;
-        this.outputText = 500;
-    }
-}
