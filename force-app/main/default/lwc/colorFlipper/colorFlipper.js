06594ee497f863d48f9c62b43d59673c1a9e8fe5
Scott Fenton <sfenton3@users.noreply.github.com>
2022-06-23 19:12:38 -0400

    added logic to coin flipper

--- force-app/main/default/lwc/colorFlipper/colorFlipper.js
+++ force-app/main/default/lwc/colorFlipper/colorFlipper.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc'
+export default class ColorFlipper extends LightningElement {
 
-export default class ColorFlipper extends LightningElement {}
+    @api red;
+    @track red;
+
+    changeColor() {
+        //this.template.querySelector("div").innerHTML("style=background-color: blue");
+        if (!this.template.querySelectorAll("div")[3].getAttribute("style")) {
+            console.log(this.template.querySelectorAll("div")[3].setAttribute("style", "background-color:red"));
+            return
+        }
+        if (this.template.querySelectorAll("div")[3].getAttribute("style") == "background-color:red") {
+            this.template.querySelectorAll("div")[3].setAttribute("style", "background-color:blue");
+        }
+    }
+
+
+
+}
