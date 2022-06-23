aa768e4b66fd5e50cb2e175645676d9426a99be8
Scott Fenton <sfenton3@users.noreply.github.com>
2022-06-23 07:23:05 -0400

    .

--- force-app/main/default/lwc/timer/timer.js
+++ force-app/main/default/lwc/timer/timer.js
@@ -2,32 +2,48 @@ import { LightningElement, track } from 'lwc';
 
 export default class Stopwatch extends LightningElement {
 
-    @track time = 1;
-    incr = 1;
-    active = false;
+    @track time;
+    count;
+    active;
     timerID;
 
+    constructor() {
+        super();
+        this.time = "00 : 00 : 00";
+        this.active = false;
+        this.incr = 1;
+        this.count = 0;
+    }
+
     //timer stores the id of the interval
-    start(event){
-        if(this.active){
+    start(event) {
+        if (this.active) {
             return;
         }
         this.active = true;
-        this.timerID = setInterval( () =>{
-            this.time += this.incr;
-        }, 100);
+        //need arrow function to use 'this' in global context 
+        this.timerID = setInterval(() => {
+            this.count++;
+            let hour = Math.floor(this.count / 3600) < 10 ? "0" + Math.floor(this.count / 3600) : Math.floor(this.count / 3600);
+            let minute = (Math.floor(this.count / 60) % 60) < 10 ? "0" + (Math.floor(this.count / 60) % 60) : (Math.floor(this.count / 60) % 60);
+            let second = (this.count % 60) < 10 ? "0" + (this.count % 60) : (this.count % 60);
+
+
+            this.time = hour + " : " + minute + " : " + second;
+
+
+        }, 1);
 
     }
-
-    stop(event){
+    stop(event) {
         clearInterval(this.timerID);
         this.active = false;
-
     }
 
-    reset(event){
+    reset(event) {
         clearInterval(this.timerID);
-        this.time = 0;
+        this.time = "00 : 00 : 00";
+        this.count = 0;
         this.active = false;
 
     }
