2711d94ba806cc9d7ac19531b45feae540094865
Scott Fenton <sfenton3@users.noreply.github.com>
2022-06-24 13:36:42 -0400

    .

--- force-app/main/default/lwc/color/color.js
+++ force-app/main/default/lwc/color/color.js
@@ -1,6 +1,6 @@
-import { LightningElement, api, track } from 'lwc'
-export default class ColorFlipper extends LightningElement {
+import { LightningElement, track } from 'lwc';
 
+export default class Color extends LightningElement {
     @track color = "";
 
     changeColor(event) {
