add3fef110f71f4973d1a7e41da93e066f6d4435
Scott Fenton <sfenton3@users.noreply.github.com>
2022-06-24 22:10:52 -0400

    Finished Counter
    Could edit html to look nicer

--- force-app/main/default/lwc/counter/counter.js
+++ force-app/main/default/lwc/counter/counter.js
@@ -1,3 +1,22 @@
 import { LightningElement } from 'lwc';
 
-export default class Counter extends LightningElement {}
+export default class Counter extends LightningElement {
+
+    counter = 0;
+
+    inc(event) {
+        this.counter++;
+    }
+
+    dec(event) {
+        this.counter--;
+    }
+
+    reset(event) {
+        this.counter = 0;
+    }
+
+
+
+
+}
