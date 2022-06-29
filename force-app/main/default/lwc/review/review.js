6a21e2bef40f69bae4c6988eee396b72064a2abc
Scott Fenton <sfenton3@users.noreply.github.com>
2022-06-29 17:35:21 -0400

    .

--- force-app/main/default/lwc/review/review.js
+++ force-app/main/default/lwc/review/review.js
@@ -1,24 +1,73 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import myimg from '@salesforce/contentAssetUrl/scott';
 
 export default class Review extends LightningElement {
 
-    name = 'scott fenton';
-    role = 'web developer';
-    tagline = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla doloribus quae molestias deleniti';
     profilephoto = myimg;
+    arrayPos = 0;
+    arrayLength = 0;
+    reviewArray;
 
+    //default object...
+    @track infoObject;
+
+    //Ideally review data is initialized through a static json file
+    constructor() {
+        super();
+        let r1 = {
+            name: 'Scott Fenton',
+            role: 'Web dev',
+            tagline: 'lorem20'
+        };
+        let r2 = {
+            name: 'Scott Benton',
+            role: 'Web traveler',
+            tagline: 'lorem30'
+        };
+        let r3 = {
+            name: 'Scott Jenton',
+            role: 'Web Extraordinary',
+            tagline: 'lorem40'
+        };
+        let r4 = {
+            name: 'Scott Senton',
+            role: 'Web Magician',
+            tagline: 'lorem50'
+        };
+
+        this.reviewArray = [r1, r2, r3, r4];
+        this.arrayPos = 0;
+        this.arrayLength = this.reviewArray.length;
+        this.infoObject = this.reviewArray[0];
+        console.log(this.reviewArray);
+    }
+
+    // remainder to wrap values around
     handlePrev() {
-        console.log('previous click');
-
+        if (this.arrayPos == 0) {
+            return;
+        }
+        this.arrayPos--;
+        this.infoObject = this.reviewArray[this.arrayPos];
     }
 
     handleNext() {
-        console.log('next click');
-
+        if (this.arrayPos == 3) {
+            return;
+        }
+        this.arrayPos++;
+        this.infoObject = this.reviewArray[this.arrayPos];
     }
 
     handleClick() {
-        console.log('suprise me');
+        let randval = Math.floor(Math.random() * (this.arrayLength));
+        while (randval == this.arrayPos) {
+            randval = Math.floor(Math.random() * (this.arrayLength));
+
+        }
+
+        this.infoObject = this.reviewArray[randval];
+        this.arrayPos = randval;
+
     }
 }
---
+++ force-app/main/default/lwc/reviewFlex/reviewFlex.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class ReviewFlex extends LightningElement {}
